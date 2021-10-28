a71be65596d4f773b0e4da25904f043831292674
PJ Rape <ptrj96@gmail.com>
2021-10-27 22:53:12 -0600

    Start using bootstrap

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import './Header.css';
+import PJ from './PJ_photo.jpg';
+import Image from 'react-bootstrap/Image';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 const data = require('./content/HeaderData');
 
 function Header() {
     return (
-        <div>
-            <header>
-                <h1> { data.name } </h1>
-                <h3> { data.title } </h3>
-                <a href = { "mailto: " + data.email } > <h3>Email</h3> </a>
-                <a href = { data.github } > <h3>GitHub</h3> </a>
-                <a href = { data.linkedin } > <h3>LinkedIn</h3> </a>
-            </header>
-        </div>
+        <Container>
+            <Image src={ PJ } className="photo" roundedCircle/>
+            <h1> { data.name } </h1>
+            <h3> { data.title } </h3>
+            <Row>
+                <Col> <a href = { "mailto: " + data.email } > Email </a> </Col>
+                <Col> <a href = { data.github } >  GitHub </a> </Col>
+                <Col> <a href = { data.linkedin } > LinkedIn </a> </Col>
+            </Row>
+        </Container>
     );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Header';
 import Jobs from './Jobs';
 import reportWebVitals from './reportWebVitals';
